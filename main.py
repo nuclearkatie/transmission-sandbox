5c22c7907dc388db3996db6d7349177e2e983e2d
Katie Mummah <radioactivekate@gmail.com>
2018-05-24 16:28:32 -0600

    update ipynb and main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from transmission_util import create_data_dict, cross_section_bounds, desired_isotopes, not_materials
 
-def main(x,transmission,error):
+def main(x,transmission,error,isotopes_to_check):
 
     data = create_data_dict('isotopes.csv')
 
     #print([d['Sigma'] for d in data])
 
     Sigma = cross_section_bounds(x,transmission,error)
+    rounded_Sigma = [round(s,5) for s in Sigma]
+    print('The range of cross sections for this material is ' + str(rounded_Sigma) + '\n' )
 
-    desired_isotope = desired_isotopes(data,'all')
+    desired_isotope = desired_isotopes(data,isotopes_to_check)
     print('The isotopes being checked are:\n' + str(desired_isotope) + '\n')
 
     outside = not_materials(data,desired_isotope,Sigma)
